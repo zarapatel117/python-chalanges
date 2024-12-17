0cd17de5fe5541dc876ae18cb985ddae92e3848c
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-17 20:44:09 +0530

    mean value

---
+++ mean value.py
@@ -0,0 +1,10 @@
+mean1=68
+correct_value=21
+wrong_value=25
+total_value=46
+sum =mean1*total_value
+print ("sum is",sum)
+y=sum-wrong_value-correct_value
+print (y)
+mean2=y/total_value
+print ("mean2 is",mean2)
